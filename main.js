8d5509962badb9587bf0873a77672540ea32b5e3
yt-world <82235868+yt-world@users.noreply.github.com>
2021-04-11 19:56:34 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const firebaseConfig = {
 apiKey: "AIzaSyCr5ZpXwEdWpQL-lZOtjRa5Nz5pY5h349o",
   authDomain: "yt-world.firebaseapp.com",
+  databaseURL: "https://yt-world-rtdb.firebaseio.com",
   projectId: "yt-world",
   storageBucket: "yt-world.appspot.com",
   messagingSenderId: "1091241197826",
   appId: "1:1091241197826:web:eceed791243e36daf3821b",
   measurementId: "G-50SGZEE3DS"
 };
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
